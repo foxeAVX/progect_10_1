1ec5b96f82e9c30284c3475660ceb784d79d137e
Zhenya <Zhenya@gmail.com>
2025-12-08 07:11:40 +1200

    End_v2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.widget import get_date, mask_account_card
 
-print(mask_account_card(input("Введите карту или счет")))
-print(get_date(str(input("Введите дату"))))
+print(mask_account_card(input("Введите карту или счет: ")))
+print(get_date(input("Введите дату: ")))
 
 # для проверки
 # Счет 35383033474447895560
--- src/masks.py
+++ src/masks.py
@@ -16,5 +16,3 @@ def get_mask_card_number(card_number: str) -> str:
 
     card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
     return card_number
-
-
